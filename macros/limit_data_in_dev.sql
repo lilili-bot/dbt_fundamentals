fc79b0016760807d9bb40b76a4c463cf2fe27fca
lili Cheng <lili.p.cheng@gmail.com>
2022-03-28 09:58:44 +0000

    adddate_has_bug

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,5 +1,5 @@
 {% macro limit_data_in_dev(column_name) %}
 {% if target.name == 'dev' %}
-where {{column_name}} >= dateadd('day',-5,current_timestamp)
+where {{column_name}} >= {{ dateadd('month',-1,'2018-03-30') }}
 {% endif %}
 {% endmacro %}
--- models/jaffle_shop/staging/stg_orders.sql
+++ models/jaffle_shop/staging/stg_orders.sql
@@ -1,13 +1,9 @@
-with orders as (
-    
+with orders as (   
     select
         id as order_id,
         user_id as customer_id,
         order_date,
         status
-
     from {{source('jaffle_shop','orders')}}
 )
-
 select * from orders
-{{ limit_data_in_dev('order_date') }}
